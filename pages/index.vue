bc62bc97546bf1390eafbc1ec4dffc3697fd98a7
Fuck off <shivascript@gmail.com>
2020-02-16 15:05:38 +0300

    first page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,51 @@
 <template>
-  <section class="container">
-     Init
-  </section>
+  <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Страница 1</v-toolbar-title>
+                <v-spacer />
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Введите целое или дробное число"
+                    name="number1"
+                    type="text"
+                  />
+
+                  <v-text-field
+                    label="Введите целое или дробное число"
+                    name="naumber2"
+                    type="text"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="primary">Далее</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
 </template>
 
 <script>
